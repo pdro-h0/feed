ac3597cb1b7ede39f741120c149dd0248a9f4bf1
pdro-h0 <pedro.pho792@gmail.com>
2023-03-03 19:38:52 -0300

    Aplicando responsividade

--- src/components/App/styled.ts
+++ src/components/App/styled.ts
@@ -10,4 +10,8 @@ export const Wrapper = styled.div`
     grid-template-columns: 256px 1fr;
     gap: 2rem;
     align-items: flex-start;
+
+    @media (max-width: 768px){
+        grid-template-columns: 1fr;
+    }
 `
